d2c1371a3ea44de511fe6b5704d0c1038cc90a8a
Ivan Balaksha <tagantroy@gmail.com>
2022-03-27 14:50:39 +0100

    rename module

--- plugin/remote-test-executor/merkle-tree/src/main/kotlin/com/tagantroy/merkletree/MerkleTree.kt
+++ plugin/remote-test-executor/merkle-tree/src/main/kotlin/com/tagantroy/merkletree/MerkleTree.kt
@@ -1,5 +1,30 @@
 package com.tagantroy.merkletree
 
+import com.tagantroy.merkletree.types.Digest
+import com.tagantroy.merkletree.types.TreeOutput
+import com.tagantroy.merkletree.types.TreeStats
+import com.tagantroy.merkletree.types.UploadInfoEntry
+
 class MerkleTree {
 
 }
+
+
+
+data class PreparedTree(val root: Digest, val inputs: List<UploadInfoEntry>, val stats: TreeStats)
+
+
+
+
+fun computeMerkleTree() {
+
+}
+
+fun flattenTree(): Map<String, TreeOutput> {
+    TODO("Not implemented yet")
+}
+
+fun computeOutputsToUpload(): Map<Digest, UploadInfoEntry> {
+    TODO("Not implemented yet")
+}
+
